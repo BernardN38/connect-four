163a007ee6287f0778a2265f1c78dc4ade851793
Bernardo Narvaez <85888299+BernardN38@users.noreply.github.com>
2021-07-06 11:53:30 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -6,51 +6,45 @@ const tokenArray = [];
 //keeps track of which players turn it is
 let turn = 'yellow';
 
-//blocks multiple click while one click is bing processed
-let freeze = false;
+//blocks multiple clicks while one click is bing processed
+let blockClicks = false;
 
-//creates a class for each slot in game and defines its properties and methods
+//creates a class for each slot in game and define its properties and methods
 class TokenSlot {
 	constructor(position, id, x, y, color = 'none', filled = false) {
 		this.position = position;
 		this.id = parseInt(id);
 		this.color = color;
 		this.filled = filled;
-		this.x = position % 7;
+		this.x = position + HEIGHT + 1;
 		this.y = parseInt(y);
 	}
+	// makes current token slot yellow and calls animation function, then switches turn
 	makeYellow() {
-		document.getElementById(this.id + 7).classList.add('yellow');
+		document.getElementById(this.id + HEIGHT + 1).classList.add('yellow');
 		animateDown(this.id, 'yellow');
 		turn = 'red';
 	}
+	// makes current token slot yellow and calls animation function, then switches turn
 	makeRed() {
-		document.getElementById(this.id + 7).classList.add('red');
+		document.getElementById(this.id + HEIGHT +1).classList.add('red');
 		animateDown(this.id, 'red');
 		turn = 'yellow';
 	}
 }
-
-assignIds();
-gameContainer.addEventListener('click', handleClick);
-
 
 //validates click and calls class method depending on which players turn
 function handleClick(event) {
 	if (event.target.classList[0] != 'col') return;
 	const id = event.target.id;
 	if (id > 6) return;
-	if (freeze) return;
-	freeze = true;
-
+	if (blockClicks) return;
+	blockClicks = true;
 	if (turn === 'yellow') return tokenArray[id].makeYellow();
 	if (turn === 'red') return tokenArray[id].makeRed();
 }
 
-
-
-
-// asign each div a unique id and pushes to tokenArray with its prperties 
+// asign each div a unique id and pushes to tokenArray with its prperties
 function assignIds() {
 	let counter = 0;
 	for (let x of gameContainer.children) {
@@ -62,62 +56,64 @@ function assignIds() {
 	}
 }
 
-
 //tracks changes in slots when a token is places. Updates propeties in tokenArray
 function updateArray(id, color) {
 	tokenArray[id].color = color;
 	tokenArray[id].filled = true;
 }
 
-
 //animates a token when it is placed in a slot and checks for winner after turn(ends game)
 function animateDown(id, color) {
-	let currentId = id + 7;
-	let nextId = id + 14;
-	let interval = setInterval(() => {
+	//sets ids for drop down animation starts at 7 to account for top row
+	let currentId = id + HEIGHT +1;
+	let nextId = id + (HEIGHT *2) +2;
+
+	let animationInterval = setInterval(() => {
+		//validates input, stops interval when reaches end of range, waits for previous animation to stop then allows next input
 		if (nextId > 48) {
 			updateArray(currentId, color);
-			freeze = false;
-			return clearInterval(interval);
+			blockClicks = false;
+			return clearInterval(animationInterval);
 		}
+
+		//stops animation if next element is filled, checks for win
 		if (
 			document.getElementById(nextId).classList[1] === 'red' ||
 			document.getElementById(nextId).classList[1] === 'yellow'
 		) {
 			updateArray(currentId, color);
-			if (checkForWinRed()) {
-				clearInterval(interval);
-				return alert('red win');
-			} else if (checkForWinYellow()) {
-				clearInterval(interval);
-				return alert('yellow win!');
+			if (checkWin('red')) {
+				clearInterval(animationInterval);
+				return alert('Red wins!');
+			} else if (checkWin('yellow')) {
+				clearInterval(animationInterval);
+				return alert('Yellow wins!');
 			}
-			freeze = false;
-			return clearInterval(interval);
+			blockClicks = false;
+			return clearInterval(animationInterval);
 		}
+
+		// removes color from current element and adds color to next element with css class
 		document.getElementById(currentId).classList.remove(color);
 		document.getElementById(nextId).classList.add(color);
-		nextId += 7;
-		currentId += 7;
-	}, 250);
+		//prepares ids for next iteration
+		nextId += HEIGHT + 1;
+		currentId += HEIGHT + 1;
+	}, 200);
 }
 
-
-
-
-
-
 //checks if winning conditions are met by looping over every possible winning scenerio depending on grid size
+//modified from provided code
 const WIDTH = 6;
 const HEIGHT = 6;
 
-function checkForWinRed() {
+function checkWin(color) {
 	function _win(cells) {
 		// Check four cells to see if they're all color of current player
 		//  - cells: list of four (y, x) cells
 		//  - returns true if all are legal coordinates & all match currPlayer
 		return cells.every(
-			([ y, x ]) => y >= 0 && y <= HEIGHT && x >= 0 && x <= WIDTH && tokenArray[y * 7 + x].color === 'red'
+			([ y, x ]) => y >= 0 && y <= HEIGHT && x >= 0 && x <= WIDTH && tokenArray[y * 7 + x].color === color
 		);
 	}
 
@@ -137,29 +133,11 @@ function checkForWinRed() {
 		}
 	}
 }
-function checkForWinYellow() {
-	function _win(cells) {
-		// Check four cells to see if they're all color of current player
-		//  - cells: list of four (y, x) cells
-		//  - returns true if all are legal coordinates & all match currPlayer
-		return cells.every(
-			([ y, x ]) => y >= 0 && y <= HEIGHT && x >= 0 && x <= WIDTH && tokenArray[y * 7 + x].color === 'yellow'
-		);
-	}
 
-	for (let y = 0; y <= HEIGHT; y++) {
-		for (let x = 0; x <= WIDTH; x++) {
-			// get "check list" of 4 cells (starting here) for each of the different
-			// ways to win
-			const horiz = [ [ y, x ], [ y, x + 1 ], [ y, x + 2 ], [ y, x + 3 ] ];
-			const vert = [ [ y, x ], [ y + 1, x ], [ y + 2, x ], [ y + 3, x ] ];
-			const diagDR = [ [ y, x ], [ y + 1, x + 1 ], [ y + 2, x + 2 ], [ y + 3, x + 3 ] ];
-			const diagDL = [ [ y, x ], [ y + 1, x - 1 ], [ y + 2, x - 2 ], [ y + 3, x - 3 ] ];
 
-			// find winner (only checking each win-possibility as needed)
-			if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-				return true;
-			}
-		}
-	}
+function init(){
+	assignIds();
+	gameContainer.addEventListener('click', handleClick);
 }
+
+init()
